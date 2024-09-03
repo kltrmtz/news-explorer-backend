9736401c13b22c1586504a15a12b4e6cc278f933
Kolt Metz <kltrmtz@gmail.com>
2024-09-03 17:37:17 -0400

    Update route error

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const { getCurrentUser, updateProfile } = require("../controllers/users");
+const { getCurrentUser } = require("../controllers/users");
 const { auth } = require("../middlewares/auth");
 const { validateUserBodyUpdate } = require("../middlewares/validation");
 
 router.use(auth);
 
 router.get("/me", getCurrentUser);
-router.patch("/me", validateUserBodyUpdate, updateProfile);
+router.patch("/me", validateUserBodyUpdate);
 
 module.exports = router;
