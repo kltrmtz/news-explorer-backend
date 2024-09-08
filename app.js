f0afec587ada78f8ba10cdb92c5e2f733a9fabaa
Kolt Metz <kltrmtz@gmail.com>
2024-09-08 18:53:15 -0400

    env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
+require("dotenv").config();
 
 const { MONGODB_CONNECT } = require("./utils/config");
 
